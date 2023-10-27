4c14d5ab2f9797aed9f698d55a17e178fc504d27
Juan Zamudio <jdzamudio5@gmail.com>
2023-10-27 11:51:37 -0500

    x

--- src/main/java/org/semaforojdz/Main.java
+++ src/main/java/org/semaforojdz/Main.java
@@ -3,11 +3,11 @@ package org.semaforojdz;
 public class Main {
     public static void main(String[] args) {
         String colorSemaforo="Amarillo";
-        if (colorSemaforo == "Verde") {
+        if (colorSemaforo.equals("Verde")) {
             System.out.println("Puede pasar");
-        } else if (colorSemaforo=="Amarillo") {
+        } else if (colorSemaforo.equals("Amarillo")) {
             System.out.println("Prepárese para frenar");
-        } else if (colorSemaforo=="Rojo") {
+        } else if (colorSemaforo.equals("Rojo")) {
             System.out.println("Debe detenerse");
         } else {
             System.out.println("No estás en un semáforo");
