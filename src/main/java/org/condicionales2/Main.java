fbe0d7e32ff373fb19d6ee5aec0ddf9e9a02c282
Juan Zamudio <jdzamudio5@gmail.com>
2023-10-30 10:29:17 -0500

    x

---
+++ src/main/java/org/condicionales2/Main.java
@@ -0,0 +1,24 @@
+package org.condicionales2;
+
+import javax.sound.midi.Soundbank;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String user="";//Occidente
+        String password="";//Occidente*2023
+        
+        user=JOptionPane.showInputDialog("Ingresa tu usuario");
+        password=JOptionPane.showInputDialog("Ingresa tu contraseña");
+        
+        if (user.equals("Occidente")&& password.equals("Occidente*2023")){
+            JOptionPane.showMessageDialog(null, "Bienvenido, te has logueado.");
+            } else if (user != "Occidente" || password != "Occidente*2023") {
+            JOptionPane.showMessageDialog(null, "Usuario o contraseña errada");
+        } else {
+            System.out.println("Error");
+        }
+
+
+    }
+}
