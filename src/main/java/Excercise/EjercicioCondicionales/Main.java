df4b7de36ead874237487f031dc0b99fccbc5031
Gustavo Jimenez <tavodres@gmail.com>
2023-10-30 10:34:15 -0500

    ejercicio login

---
+++ src/main/java/Excercise/EjercicioCondicionales/Main.java
@@ -0,0 +1,32 @@
+package Excercise.EjercicioCondicionales;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String usuario,clave;
+
+        usuario= JOptionPane.showInputDialog("Introduce el nombre de usuario");
+
+
+
+        if(usuario.equals("Occidente")){
+            clave= JOptionPane.showInputDialog("Introduce la clave");
+
+            if(clave.equals("Occidente*2023")){
+                JOptionPane.showMessageDialog(null, "Bienvenido, te has logueado.");
+
+            }else{
+                JOptionPane.showMessageDialog(null, "Clave Incorrecta");}
+
+        } else {
+            JOptionPane.showMessageDialog(null, "El nombre de usuario no existe");;}
+
+
+    }
+
+
+
+}
