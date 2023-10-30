641b290fcaf754f21ff60ab06210bd4e1ed67981
Eduardo Rodriguez <joedroav@hotmail.com>
2023-10-30 14:53:44 -0500

    Desarrollo tablas Multiplicar

---
+++ src/main/java/org/Tabladel7/Main.java
@@ -0,0 +1,11 @@
+package org.Tabladel7;
+
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println("Tabla del 7");
+        for(i=0; i<=10; i++){
+                System.out.println("7" +"*" + i +"="+7*i);
+        }
+    }
+}
