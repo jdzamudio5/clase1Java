70958e3c2c1aca933d0950e06d299323e2f677dd
Juan Zamudio <jdzamudio5@gmail.com>
2023-10-31 11:13:43 -0500

    quiz

--- src/main/java/org/whilejdz/Main.java
+++ src/main/java/org/whilejdz/Main.java
@@ -1,4 +1,14 @@
 package org.whilejdz;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int i=1;
+
+        while (i<11){
+            int mul=2*i;
+            System.out.println(mul);
+            i++;
+        }
+    }
 }
