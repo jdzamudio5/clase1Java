1834335b89680c1c9b01c4fba03c00f47ac9e3c7
Alejandro Oliveros Castro <alejandrop758@gmail.com>
2023-10-31 10:13:04 -0500

    Tabla del multiplicar

---
+++ src/main/java/Condicional3/Condicional3.java
@@ -0,0 +1,12 @@
+package Condicional3;
+
+public class Condicional3 {
+    public static void main(String[] args) {
+        int numero =7;
+        System.out.println("Tabla del 7:");
+        for(int i=1; i<=10; i++){
+            int res = numero* i;
+            System.out.println(numero+" X "+ i+" = "+ res);
+        }
+    }
+}
