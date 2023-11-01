02846d3e2ec149bf480ed7b4094a9855823a2ca5
SQARafael <rafael.chica@sqasa.co>
2023-11-01 12:07:36 -0500

    refactor

--- src/main/java/co/rafael/example/Main2.java
+++ src/main/java/co/rafael/example/Main2.java
@@ -1,4 +1,4 @@
-package org.example;
+package co.rafael.example;
 
 public class Main2 {
     public static void main(String[] args) {
--- src/main/java/co/rafael/exercise/Main.java
+++ src/main/java/co/rafael/exercise/Main.java
@@ -1,4 +1,4 @@
-package org.exercise;
+package co.rafael.exercise;
 /*
  * @(#) Main.java 11.0
  * Copyright 2023 SQA. Todos los derechos reservados.
