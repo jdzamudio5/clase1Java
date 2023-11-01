0a761e56758acea10aa692b5e68764486d9e6ce5
Juan Zamudio <jdzamudio5@gmail.com>
2023-10-31 21:35:26 -0500

    Quiz mejorado

--- src/main/java/org/quizjdz/Array.java
+++ src/main/java/org/quizjdz/Array.java
@@ -4,17 +4,8 @@ import javax.swing.*;
 
 public class Array {
     public static void main(String[] args) {
-        int[] enteros=new int[10];
-        enteros[0] =5;
-        enteros[1] =10;
-        enteros[2] =3;
-        enteros[3] =8;
-        enteros[4] =15;
-        enteros[5] =7;
-        enteros[6] =2;
-        enteros[7] =9;
-        enteros[8] =1;
-        enteros[9] =6;
+        int[] enteros={5,10,3,8,15,7,2,9,1,6};
+
         int suma=0;
         for (int i = 0; i < enteros.length; i++) {
             suma=suma+enteros[i];
