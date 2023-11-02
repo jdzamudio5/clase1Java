5d7c7e2ee1a95be337d69d8857e5e75aec0f7c6d
IngjavierD <90328565+IngjavierD@users.noreply.github.com>
2023-11-02 11:35:06 -0500

    Actulizacion 2 de Novi

---
+++ src/main/java/co_Javier_Diaz/Usuarios/Usuario.java
@@ -0,0 +1,23 @@
+package co_Javier_Diaz.Usuarios;
+
+public class Usuario {
+    private String nombre;
+    private String contrasena;
+
+    public Usuario(String nombre, String contrasena) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String nuevaContrasena) {
+        this.contrasena = nuevaContrasena;
+    }
+}
