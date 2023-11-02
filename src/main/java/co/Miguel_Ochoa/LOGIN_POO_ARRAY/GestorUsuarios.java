0e7070a372d24b39eb41fa2fa7d929eeab43f437
panda-11128a <149093934+panda-11128a@users.noreply.github.com>
2023-11-02 12:28:55 -0500

    Login Array

---
+++ src/main/java/co/Miguel_Ochoa/LOGIN_POO_ARRAY/GestorUsuarios.java
@@ -0,0 +1,131 @@
+package co.Miguel_Ochoa.LOGIN_POO_ARRAY;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorUsuarios {
+    private List<Usuario> usuarios;
+
+    public GestorUsuarios() {
+        this.usuarios = new ArrayList<>();
+    }
+
+    public void registrarUsuario() {
+        String nombreCompleto = JOptionPane.showInputDialog("Ingrese su nombre completo:");
+        String cedula = JOptionPane.showInputDialog("Ingrese su cédula:");
+        String correo = JOptionPane.showInputDialog("Ingrese su correo electronico:");
+        String nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
+        String contrasena = JOptionPane.showInputDialog("Ingrese su contrasena:");
+
+        Usuario usuario = new Usuario(nombreCompleto, cedula, correo, nombreUsuario, contrasena);
+        usuarios.add(usuario);
+        JOptionPane.showMessageDialog(null, "Usuario registrado exitosamente!");
+        mostrarMenu();
+    }
+
+    public void iniciarSesion() {
+        String nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
+        String contrasena = JOptionPane.showInputDialog("Ingrese su contrasena:");
+
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNombreUsuario().equals(nombreUsuario) && usuario.getcontrasena().equals(contrasena)) {
+                JOptionPane.showMessageDialog(null, "Bienvenido, " + nombreUsuario + "!");
+                mostrarMenu();
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Nombre de usuario o contrasena incorrectos.");
+        mostrarMenu();
+    }
+
+    public void actualizarInformacionUsuario() {
+        String nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario para actualizar:");
+
+        for (Usuario usuario : usuarios) {
+            if (usuario.getNombreUsuario().equals(nombreUsuario)) {
+                String nuevoCorreo = JOptionPane.showInputDialog("Ingrese el nuevo correo electronico:", usuario.getCorreo());
+                usuario.setCorreo(nuevoCorreo);
+
+                String nuevacontrasena = JOptionPane.showInputDialog("Ingrese la nueva contrasena:", usuario.getcontrasena());
+                usuario.setcontrasena(nuevacontrasena);
+
+                JOptionPane.showMessageDialog(null, "Informacion actualizada con éxito.");
+                mostrarMenu();
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Usuario no encontrado.");
+        mostrarMenu();
+    }
+
+    public void eliminarUsuario() {
+        if (usuarios.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No hay usuarios registrados.");
+        } else {
+            String nombreUsuario = JOptionPane.showInputDialog("Ingrese el nombre de usuario para eliminar:");
+
+            boolean eliminado = usuarios.removeIf(usuario -> usuario.getNombreUsuario().equals(nombreUsuario));
+
+            if (eliminado) {
+                JOptionPane.showMessageDialog(null, "Usuario eliminado exitosamente.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Usuario no encontrado.");
+            }
+        }
+        mostrarMenu();
+    }
+
+    public void verUsuariosRegistrados() {
+        if (usuarios.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No hay usuarios registrados.");
+        } else {
+            StringBuilder listaUsuarios = new StringBuilder("Usuarios registrados:\n");
+            for (Usuario usuario : usuarios) {
+                listaUsuarios.append(usuario.toString()).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, listaUsuarios.toString());
+        }
+        mostrarMenu();
+    }
+
+    public void mostrarMenu() {
+        String[] opciones = {
+                "Registrar",
+                "Iniciar Sesion",
+                "Actualizar informacion de usuario",
+                "Eliminar datos de usuario",
+                "Ver usuarios registrados",
+                "Salir"
+        };
+        int seleccion = JOptionPane.showOptionDialog(
+                null, "Seleccione una opcion", "Menú",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                null, opciones, opciones[0]
+        );
+
+        switch (seleccion) {
+            case 0:
+                registrarUsuario();
+                break;
+            case 1:
+                iniciarSesion();
+                break;
+            case 2:
+                actualizarInformacionUsuario();
+                break;
+            case 3:
+                eliminarUsuario();
+                break;
+            case 4:
+                verUsuariosRegistrados();
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null, "Saliendo...");
+                System.exit(0);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opcion no válida.");
+                mostrarMenu();
+        }
+    }
+}
---
+++ src/main/java/co/Miguel_Ochoa/LOGIN_POO_ARRAY/Usuario.java
@@ -0,0 +1,71 @@
+package co.Miguel_Ochoa.LOGIN_POO_ARRAY;
+
+public class Usuario {
+    private String nombreCompleto;
+    private String cedula;
+    private String correo;
+    private String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(String nombreCompleto, String cedula, String correo, String nombreUsuario, String contrasena) {
+        this.nombreCompleto = nombreCompleto;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    // Métodos getters y setters para cada campo
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getcontrasena() {
+        return contrasena;
+    }
+
+    public void setcontrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombreCompleto='" + nombreCompleto + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", correo='" + correo + '\'' +
+                ", nombreUsuario='" + nombreUsuario + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                '}';
+    }
+}
+
