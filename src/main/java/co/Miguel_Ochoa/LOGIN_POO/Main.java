d4c7c6108d14f274f58cd7ff2f3a2c5e90f8c7fe
panda-11128a <149093934+panda-11128a@users.noreply.github.com>
2023-11-02 10:58:59 -0500

    Ejercicio 1 Clase 2 Noviembre

--- src/main/java/co/Miguel_Ochoa/LOGIN_POO/Main.java
+++ src/main/java/co/Miguel_Ochoa/LOGIN_POO/Main.java
@@ -3,16 +3,16 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         // Credenciales fijas
-        String username = "Occidente*2023";
-        String password = "Occidente";
+        String username = "Occidente";
+        String password = "Occidente2023*";
 
         // Crear el usuario con las credenciales
         Usuario usuario = new Usuario(username, password);
         SistemaDeLogin sistemaDeLogin = new SistemaDeLogin(usuario);
 
         // Pedir al usuario que inicie sesión
-        String loginUsername = JOptionPane.showInputDialog(null, "Inicio de sesión\nIngrese su nombre de usuario:");
-        String loginPassword = JOptionPane.showInputDialog(null, "Ingrese su contraseña:");
+        String loginUsername = JOptionPane.showInputDialog(null, "Inicio de sesion\nIngrese su nombre de usuario:");
+        String loginPassword = JOptionPane.showInputDialog(null, "Ingrese su contrasena:");
 
         // Validar las credenciales
         if (sistemaDeLogin.iniciarSesion(loginUsername, loginPassword)) {
