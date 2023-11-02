ee68ee493206d5b1d0ee7a44313b9fcb6f8def86
SQARafael <rafael.chica@sqasa.co>
2023-11-02 12:05:18 -0500

    Ajuste de errores

--- src/main/java/co/Alejandra/Condicionales_clase2/Main.java
+++ src/main/java/co/Alejandra/Condicionales_clase2/Main.java
@@ -1,14 +1,8 @@
-<<<<<<<< HEAD:src/main/java/co/Alejandra/Condicionales_clase2/Main.java
+
 package co.Alejandra.Condicionales_clase2;
 import javax.swing.JOptionPane;
 import java.util.Objects;
-public class Main {
-========
-package co.Javier_Diaz.Condiccionales_clase2_ValidarUsuario;
->>>>>>>> f480100fe0edb5075d1b7a2286e90bbbc21ff376:src/main/java/co/Javier_Diaz/Condiccionales_clase2_ValidarUsuario/Main.java
 
-import javax.swing.*;
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
