6e893507d346766cfc680d64d6e6924dfaefad54
Eduardo Rodriguez <joedroav@hotmail.com>
2023-11-06 22:06:19 -0500

    Ejercicio login

---
+++ src/main/java/co/jorge/Login/Agregar.java
@@ -0,0 +1,13 @@
+package co.jorge.Login;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Agregar {
+    public static void nuevoUsuario(ArrayList<String> usuarios){
+        String user = JOptionPane.showInputDialog("Ingrese el nuevo usuario");
+        String clave = JOptionPane.showInputDialog("Ingrese la clave del nuevo usuario");
+        usuarios.add(user+clave);
+        System.out.println(usuarios);
+    }
+}
--- src/main/java/co/jorge/Login/Main.java
+++ src/main/java/co/jorge/Login/Main.java
@@ -1,18 +1,57 @@
 package co.jorge.Login;
 
 import javax.swing.*;
+
 import java.util.ArrayList;
+
+import static co.jorge.Login.Agregar.nuevoUsuario;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> usuarios = new ArrayList<String>();
+        usuarios.add("admin"+"admin");
+        int i=0;
+        do {
+            String user = JOptionPane.showInputDialog("Ingrese el usuario administrador");
+            String clave = JOptionPane.showInputDialog("Ingrese la clave del administrador");
+            if(usuarios.contains(user+clave)){
+                i=1;
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Error, intente de nuevo!!!");
+            }
+        }while(i==0);
+        if(i==1){
+            String[] opciones = {"Agregar", "Eliminar","Imprimir", "Salir"};
+            int seleccion = JOptionPane.showOptionDialog(
+                    null,
+                    "Selecciona una opcion",
+                    "Opciones",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    opciones,
+                    opciones[0]
+            );
+            if (seleccion == 0) {
+                // Opción Agregar seleccionada
+                System.out.println("Agregar");
+                nuevoUsuario(usuarios);
 
-        guardarUsuario("rchica","Occidente2023");
+            } else if (seleccion == 1) {
+                // Opción Eliminar seleccionada
+                System.out.println("Eliminar");
+            } else if (seleccion == 2) {
+                // Opción imprimir seleccionada
+                System.out.println("Imprimir");
+                //imprimirContenido(usuario);
 
-        Login login=new Login();
-        String user= JOptionPane.showInputDialog("Ingrese el usuario");
-        String clave=JOptionPane.showInputDialog("Ingrese la clave");
-        login.validacionCredenciales(user,clave);
-
-
+            } else if (seleccion == 3) {
+                // Opción Salir seleccionada
+                System.out.println("Hasta Luego!!!");
+            }
+        }
     }
 }
+
+
