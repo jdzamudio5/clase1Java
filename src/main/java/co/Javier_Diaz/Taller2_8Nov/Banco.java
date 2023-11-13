64e0dc7a13c174be0468697266f8736f9fdb3a9d
IngjavierD <90328565+IngjavierD@users.noreply.github.com>
2023-11-12 20:51:35 -0500

    Actualizacion Trabajo Final Java version 2.0

--- src/main/java/co/Javier_Diaz/Taller2_8Nov/Banco.java
+++ src/main/java/co/Javier_Diaz/Taller2_8Nov/Banco.java
@@ -2,6 +2,7 @@ package co.Javier_Diaz.Taller2_8Nov;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Banco{
     private ArrayList<Cliente> Datos = new ArrayList<>();
@@ -13,13 +14,53 @@ public class Banco{
     }
 
     public void requerirDatosNuevacuenta(){
-        int tempCuenta= (int) Math.random()+999;
-        double tempsaldo=50000;
+
+        String Numero_documento=(JOptionPane.showInputDialog("Identificacion:"));
         String Nombres=(JOptionPane.showInputDialog("Nombres:"));
         String Apellidos=(JOptionPane.showInputDialog("Apellidos:"));
-        String Numero_documento=(JOptionPane.showInputDialog("Identificacion:"));
         String Usuarios=(JOptionPane.showInputDialog("Usuario:"));
-        String Contrasenas=(JOptionPane.showInputDialog("contraseña:"));
+        String Contrasenas=(JOptionPane.showInputDialog("contrasena:"));
+
+        //Creacion del Numero Aleatorio
+        Random random = new Random();
+        int tempCuenta= random.nextInt(1000);
+
+        //Creacion del Saldo - Se crar opcion por defecto o diferente valor - Si es diferente monto valida que sea mayor al minimo seleccionado.
+        double tempsaldo=50000;
+        int opcion_MontoInicial;
+        String[] opciones_MontoInicial = {"Monto Por Defecto "+tempsaldo, "Diferente Monto"};
+        opcion_MontoInicial = JOptionPane.showOptionDialog(
+                null,
+                "Selecciona una opcion:",
+                "Recuerda que el Monto debe ser igual o Mayor a: "+tempsaldo,
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opciones_MontoInicial,
+                opciones_MontoInicial[0]);
+
+        switch (opcion_MontoInicial)
+        {
+            case 0:
+                JOptionPane.showMessageDialog(null, "Monto Minimo Inicial de "+tempsaldo);
+                break;
+            case 1:
+                boolean Validador_Monto_Monimo=true;
+                do{
+                    double nuevotempsaldo= Double.parseDouble((JOptionPane.showInputDialog("Ingrese Monto: ")));
+                    if (nuevotempsaldo>=tempsaldo){
+                        tempsaldo=nuevotempsaldo;
+                        Validador_Monto_Monimo=false;
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(null, "Ingrese un Monto Mayor o igual a  "+tempsaldo);
+                    }
+                }while(Validador_Monto_Monimo);
+
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opcion no valida");
+        }
 
         Cuenta cuenta = new Cuenta(tempCuenta,tempsaldo);
         Cliente cliente = new Cliente(Nombres,Apellidos,Numero_documento,cuenta,Usuarios,Contrasenas);
@@ -27,9 +68,165 @@ public class Banco{
     }
 
     public void verClientes(){
-        for (int i=0;i<Datos.size();i++){
-            System.out.println(Datos.get(i).getCuenta().getNumero_Cuenta());
+
+        // Crear un StringBuilder para almacenar las iteraciones
+        StringBuilder mensajeBuilder = new StringBuilder();
+
+        for (int i = 0; i < Datos.size(); i++) {
+            mensajeBuilder.append("Nombres: ").append(Datos.get(i).getNombres()).append("\n");
+            mensajeBuilder.append("Apellidos: ").append(Datos.get(i).getApellidos()).append("\n");
+            mensajeBuilder.append("Numero de Documento: ").append(Datos.get(i).getNumero_Documento()).append("\n");
+            mensajeBuilder.append("Usuario: ").append(Datos.get(i).getUsuario()).append("\n");
+            mensajeBuilder.append("Contrasena: ").append(Datos.get(i).getContrasena()).append("\n");
+            mensajeBuilder.append("Numero de Cuenta: ").append(Datos.get(i).getCuenta().getNumero_Cuenta()).append("\n");
+            mensajeBuilder.append("Saldo: ").append(Datos.get(i).getCuenta().getSaldo()).append("\n");
+            mensajeBuilder.append("-------------------------------------------------------------------------------------\n\n");
+        }
+
+        // Obtener el String final
+        String mensajeFinal = mensajeBuilder.toString();
+
+        JOptionPane.showMessageDialog(null, mensajeFinal);
+    }
+    public void Deposito(){
+        int Numero_Cuenta = -1;
+        String Numero_identificacion="Null";
+
+        int opcion_Deposito;
+        String[] opciones_Deposito = {"Numero de Cuenta", "Numero de Documento"};
+        opcion_Deposito = JOptionPane.showOptionDialog(
+                null,
+                "Selecciona una opcion:",
+                "Banco De Occidente",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opciones_Deposito,
+                opciones_Deposito[0]);
+
+        switch (opcion_Deposito){
+            case 0:
+                Numero_Cuenta= Integer.parseInt((JOptionPane.showInputDialog("Numero De Cuenta:")));
+                for (int i = 0; i < Datos.size(); i++) {
+
+                    if (Numero_Cuenta == Datos.get(i).getCuenta().getNumero_Cuenta()) {
+                        double Deposito = Double.parseDouble((JOptionPane.showInputDialog("Ingrese Monto a Depositar: ")));
+                        double NuevoSaldo=Datos.get(i).getCuenta().getSaldo() + Deposito;
+                        Datos.get(i).getCuenta().setSaldo(NuevoSaldo);
+                        JOptionPane.showMessageDialog(null, "El nuevo Saldo para cuenta: "+Datos.get(i).getCuenta().getNumero_Cuenta()+" es de "+NuevoSaldo);
+                    }
+                }
+                break;
+
+                case 1:
+                    Numero_identificacion=(JOptionPane.showInputDialog("Numero De identificacion:"));
+                    ArrayList<Integer> CuentaIdentificacion = new ArrayList<>();
+
+                    for (int i = 0; i < Datos.size(); i++) {
+                        if (Numero_identificacion.equals(Datos.get(i).getNumero_Documento())){
+                            CuentaIdentificacion.add(Datos.get(i).getCuenta().getNumero_Cuenta());
+                        }
+                    }
+
+                    Integer[] arrayCuentas = CuentaIdentificacion.toArray(new Integer[CuentaIdentificacion.size()]);
+
+                    int opcion_Cuenta_Deposito;
+                    int Cuenta_opcion_Deposito = JOptionPane.showOptionDialog(
+                            null,
+                            "Selecciona una Cuenta:",
+                            "Banco De Occidente",
+                            JOptionPane.DEFAULT_OPTION,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null,
+                            arrayCuentas,
+                            arrayCuentas[0]);
+
+
+                    for (int i = 0; i < Datos.size(); i++) {
+                        if (arrayCuentas[Cuenta_opcion_Deposito] == Datos.get(i).getCuenta().getNumero_Cuenta()) {
+                                         double Deposito = Double.parseDouble((JOptionPane.showInputDialog("Ingrese Monto a Depositar: ")));
+                                         double NuevoSaldo=Datos.get(i).getCuenta().getSaldo()+Deposito;
+                                         Datos.get(i).getCuenta().setSaldo(NuevoSaldo);
+                                         JOptionPane.showMessageDialog(null, "El nuevo Saldo para cuenta: "+Datos.get(i).getCuenta().getNumero_Cuenta()+" es de "+NuevoSaldo);
+                        }
+                    }
+
+                    break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Opcion no valida");
         }
     }
+    public void Retiro(){
+
+        {
+            int Numero_Cuenta = -1;
+            String Numero_identificacion="Null";
+
+            int opcion_Retiro;
+            String[] opciones_Retiro = {"Numero de Cuenta", "Numero de Documento"};
+            opcion_Retiro = JOptionPane.showOptionDialog(
+                    null,
+                    "Selecciona una opcion:",
+                    "Banco De Occidente",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    opciones_Retiro,
+                    opciones_Retiro[0]);
+
+            switch (opcion_Retiro){
+                case 0:
+                    Numero_Cuenta= Integer.parseInt((JOptionPane.showInputDialog("Numero De Cuenta:")));
+                    for (int i = 0; i < Datos.size(); i++) {
+                        if (Numero_Cuenta == Datos.get(i).getCuenta().getNumero_Cuenta()) {
+                            double Retiro = Double.parseDouble((JOptionPane.showInputDialog("Ingrese Monto a Retirar:")));
+                            double NuevoSaldo=Datos.get(i).getCuenta().getSaldo() - Retiro;
+                            Datos.get(i).getCuenta().setSaldo(NuevoSaldo);
+                            JOptionPane.showMessageDialog(null, "El nuevo Saldo para cuenta: "+Datos.get(i).getCuenta().getNumero_Cuenta()+" es de "+NuevoSaldo);
+                        }
+                    }
+                    break;
+
+                case 1:
+                    Numero_identificacion=(JOptionPane.showInputDialog("Numero De identificacion:"));
+                    ArrayList<Integer> CuentaIdentificacion = new ArrayList<>();
+
+                    for (int i = 0; i < Datos.size(); i++) {
+                        if (Numero_identificacion.equals(Datos.get(i).getNumero_Documento())){
+                            CuentaIdentificacion.add(Datos.get(i).getCuenta().getNumero_Cuenta());
+                        }
+                    }
+
+                    Integer[] arrayCuentas = CuentaIdentificacion.toArray(new Integer[CuentaIdentificacion.size()]);
+
+                    int opcion_Cuenta_Retiro;
+                    int Cuenta_opcion_Retiro = JOptionPane.showOptionDialog(
+                            null,
+                            "Selecciona una Cuenta:",
+                            "Banco De Occidente",
+                            JOptionPane.DEFAULT_OPTION,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null,
+                            arrayCuentas,
+                            arrayCuentas[0]);
+
+
+                    for (int i = 0; i < Datos.size(); i++) {
+                        if (arrayCuentas[Cuenta_opcion_Retiro] == Datos.get(i).getCuenta().getNumero_Cuenta()) {
+                            double Retiro = Double.parseDouble((JOptionPane.showInputDialog("Ingrese Monto a Retirar: ")));
+                            double NuevoSaldo=Datos.get(i).getCuenta().getSaldo()-Retiro;
+                            Datos.get(i).getCuenta().setSaldo(NuevoSaldo);
+                            JOptionPane.showMessageDialog(null, "El nuevo Saldo para cuenta: "+Datos.get(i).getCuenta().getNumero_Cuenta()+" es de "+NuevoSaldo);
+                        }
+                    }
+
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+        }
+}
 }
 
--- src/main/java/co/Javier_Diaz/Taller2_8Nov/Main.java
+++ src/main/java/co/Javier_Diaz/Taller2_8Nov/Main.java
@@ -1,9 +1,42 @@
 package co.Javier_Diaz.Taller2_8Nov;
 import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
-            Banco banco = new Banco();
-            banco.requerirDatosNuevacuenta();
-            banco.verClientes();
+        Banco banco = new Banco();
+
+        int opcion;
+        do {
+            String[] opciones = {"Salir", "Crear Cliente", "Depositar", "Retirar", "Ver todos los clientes"};
+            opcion = JOptionPane.showOptionDialog(
+                    null,
+                    "Selecciona una opcion:",
+                    "Menu Banco de Occidente",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    opciones,
+                    opciones[0]);
+
+            switch (opcion) {
+                case 1:
+                    banco.requerirDatosNuevacuenta();
+                    break;
+                case 2:
+                    banco.Deposito();
+                    break;
+                case 3:
+                    banco.Retiro();
+                    break;
+                case 4:
+                    banco.verClientes();
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Saliendo...");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+        } while (opcion != 0); // La opción 0 es Salir
         }
 }
